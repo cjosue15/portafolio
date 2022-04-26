ff39bda14573975b4a29bc08a6c958a697d18a7a
Carlos Josue <carlosjosuemn98@gmail.com>
2022-04-26 17:43:59 -0500

    add tabs in home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { ProfileModule } from './components/profile/profile.module';
 import { ButtonModule } from '../shared/components/button/button.module';
+import { TabsModule } from '../shared/components/tabs/tabs.module';
+import { TabItemModule } from '../shared/components/tabs/tab-item/tab-item.module';
+import { LoaderModule } from '../shared/components/loader/loader.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, ProfileModule, ButtonModule],
+  imports: [
+    CommonModule,
+    ProfileModule,
+    ButtonModule,
+    TabsModule,
+    TabItemModule,
+    LoaderModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomeModule {}
